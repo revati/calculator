bf0eb139e60751a0f408d30d11828079de47e5ec
Roberts Guļāns <roberts@rego.lv>
2016-03-22 16:14:41 +0200

    Html/sort of css

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
 (function($) {
-  var calculateValue = function(e) {
-    e.preventDefault();
-    var oldValue = $("#result").text();
-    var input = $("[name=\"equation\"]").val();
 
-    try {
-      var value = eval( input );
-      $("#result").text( value );
-    }
-    catch(e) {
-      $("#result").text( oldValue );
-    }
-  };
 
-  // Add listener
-  $("form").on("submit", calculateValue);
-  $("[name=\"equation\"]").on("keyup", calculateValue);
 })(jQuery);
